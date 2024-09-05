9a0e9b6c5ca8dbb338ca7c3ae82fc44908ce5d08
Jordan Harband <ljharb@gmail.com>
2024-09-05 11:26:30 -0700

    [Tests] dynamically read fixture dirs

--- test/index.mjs
+++ test/index.mjs
@@ -5,7 +5,7 @@ import inspect from 'object-inspect';
 import tmp from 'tmp';
 
 import { execSync, spawnSync } from 'child_process';
-import { readFile, truncate } from 'fs/promises';
+import { readFile, readdir, truncate } from 'fs/promises';
 import { join, relative, resolve } from 'path';
 
 import pruneFootnotes from '../index.mjs';
@@ -39,50 +39,54 @@ test('pruneFootnotes', async (t) => {
 	const tmpFile = tmp.fileSync({ discardDescriptor: true, postfix: '.md' });
 	t.teardown(() => tmpFile.removeCallback());
 
-	await Promise.all([
-		'changelog',
-	].map(async (fixtureName) => {
-		const inPath = `./test/fixtures/${fixtureName}/in.md`;
-		const expected = (await readFile(join(import.meta.dirname, `./fixtures/${fixtureName}/out.md`), 'utf-8')).trim();
+	await Promise.all((await readdir(join(import.meta.dirname, 'fixtures'), { withFileTypes: true }))
+		.filter((x) => x.isDirectory())
+		.map(async ({ name: fixtureName }) => {
+			t.test(`fixture: ${fixtureName}`, async (st) => {
+				const inPath = `./test/fixtures/${fixtureName}/in.md`;
+				const expected = (await readFile(join(import.meta.dirname, `./fixtures/${fixtureName}/out.md`), 'utf-8')).trim();
 
-		const relativePath = spawnSync(bin, ['-i', inPath], { stdio: 'pipe' });
-		t.equal(relativePath.status, 0, 'exits with a zero status code when a relative path input file is provided');
-		t.equal(`${relativePath.stdout}`.trim(), expected.trim());
-		t.equal(`${relativePath.stderr}`, '', 'stderr is empty');
+				const relativePath = spawnSync(bin, ['-i', inPath], { stdio: 'pipe' });
+				st.equal(relativePath.status, 0, 'exits with a zero status code when a relative path input file is provided');
+				st.equal(`${relativePath.stdout}`.trim(), expected.trim());
+				st.equal(`${relativePath.stderr}`, '', 'stderr is empty');
 
-		const absolute = spawnSync(bin, ['-i', resolve(inPath)], { stdio: 'pipe' });
-		t.equal(absolute.status, 0, 'exits with a zero status code when an absolute path input file is provided');
-		t.equal(`${absolute.stdout}`.trim(), expected.trim());
-		t.equal(`${absolute.stderr}`, '', 'stderr is empty');
+				const absolute = spawnSync(bin, ['-i', resolve(inPath)], { stdio: 'pipe' });
+				st.equal(absolute.status, 0, 'exits with a zero status code when an absolute path input file is provided');
+				st.equal(`${absolute.stdout}`.trim(), expected.trim());
+				st.equal(`${absolute.stderr}`, '', 'stderr is empty');
 
-		const stdin = spawnSync(bin, [], { stdio: 'pipe', input: await readFile(inPath, 'utf-8') });
-		t.equal(stdin.status, 0, 'exits with a zero status code when stdin input is provided');
-		t.equal(`${stdin.stdout}`.trim(), expected.trim());
-		t.equal(`${stdin.stderr}`, '', 'stderr is empty');
+				const stdin = spawnSync(bin, [], { stdio: 'pipe', input: await readFile(inPath, 'utf-8') });
+				st.equal(stdin.status, 0, 'exits with a zero status code when stdin input is provided');
+				st.equal(`${stdin.stdout}`.trim(), expected.trim());
+				st.equal(`${stdin.stderr}`, '', 'stderr is empty');
 
-		const outFile = spawnSync(bin, ['-i', inPath, '-o', tmpFile.name], { stdio: 'pipe' });
-		t.equal(outFile.status, 0, 'exits with a zero status code when an output file is provided');
-		t.equal(`${outFile.stdout}`, '', 'stdout is empty');
-		t.equal(`${outFile.stderr}`, `Wrote GFM markdown output to ${tmpFile.name}\n`, 'stderr is not empty');
-		t.equal(
-			`${await readFile(tmpFile.name, 'utf-8')}`.trim(),
-			expected,
-			'output file has expected contents',
-		);
+				const outFile = spawnSync(bin, ['-i', inPath, '-o', tmpFile.name], { stdio: 'pipe' });
+				st.equal(outFile.status, 0, 'exits with a zero status code when an output file is provided');
+				st.equal(`${outFile.stdout}`, '', 'stdout is empty');
+				st.equal(`${outFile.stderr}`, `Wrote GFM markdown output to ${tmpFile.name}\n`, 'stderr is not empty');
+				st.equal(
+					`${await readFile(tmpFile.name, 'utf-8')}`.trim(),
+					expected,
+					'output file has expected contents',
+				);
 
-		await truncate(tmpFile.name);
+				await truncate(tmpFile.name);
 
-		const relativeTmpPath = relative(process.cwd(), tmpFile.name);
-		const outRelativeFile = spawnSync(bin, ['-i', inPath, '-o', relativeTmpPath], { stdio: 'pipe' });
-		t.equal(outRelativeFile.status, 0, 'exits with a zero status code when an output file is provided');
-		t.equal(`${outRelativeFile.stdout}`, '', 'stdout is empty');
-		t.equal(`${outRelativeFile.stderr}`, `Wrote GFM markdown output to ${relativeTmpPath}\n`, 'stderr is not empty');
-		t.equal(
-			`${await readFile(tmpFile.name, 'utf-8')}`.trim(),
-			expected,
-			'output file has expected contents',
-		);
-	}));
+				const relativeTmpPath = relative(process.cwd(), tmpFile.name);
+				const outRelativeFile = spawnSync(bin, ['-i', inPath, '-o', relativeTmpPath], { stdio: 'pipe' });
+				st.equal(outRelativeFile.status, 0, 'exits with a zero status code when an output file is provided');
+				st.equal(`${outRelativeFile.stdout}`, '', 'stdout is empty');
+				st.equal(`${outRelativeFile.stderr}`, `Wrote GFM markdown output to ${relativeTmpPath}\n`, 'stderr is not empty');
+				st.equal(
+					`${await readFile(tmpFile.name, 'utf-8')}`.trim(),
+					expected,
+					'output file has expected contents',
+				);
+
+				st.end();
+			});
+		}));
 
 	t.end();
 });
